9ef216b36d1081a45c1b00d86d4b264c9d1a28a3
roee030 <roee.angel93@gmail.com>
2019-07-09 18:55:33 +0300

    NavBar
    add navbar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import {  Route, Switch } from "react-router-dom";
-import './App.css';
 
 import Home from './pages/Home';
 import SingleRoom from './pages/SingleRoom';
 import Rooms from './pages/Rooms';
+import Error from './pages/Error';
 
+import Navbar from './components/Navbar';
 function App() {
   return( 
     <>
-    <Route exact path="/" component={Home}/>
-    <Route exact path="/rooms/:slug" component={SingleRoom}/>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/rooms" component={Rooms}/>
+      <Route exact path="/single-room" component={SingleRoom}/>
+      <Route component={Error}/>
+    </Switch>
     </>
   );
 }
