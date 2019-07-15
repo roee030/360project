cf6606ec77ae388c77c55ed6ecd9faa9f4a0e226
roee030 <roee.angel93@gmail.com>
2019-07-15 20:12:01 +0300

    Update Home.js
    add Banner to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import Hero from "../components/Hero";
+import Banner from '../components/Banner';
  function Home() {
     return (
-        <Hero hero="defaultHero" />
+        <Hero hero="defaultHero">
+        <Banner title="360° Homes" subtitle="Click here to view all apartments">
+        <Link to="/rooms" className="btn-primary">
+        Our Homes
+        </Link>
+        </Banner>
+        </Hero>
     );
 }
 export default Home;
