803ff1bbb408f1ef237b4c6fe46eae664637da46
roee030 <roee.angel93@gmail.com>
2019-07-15 21:02:46 +0300

    Update Services.js
    add our site services

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,32 +1,32 @@
 import React, { Component } from "react";
-import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
+import { FaVrCardboard, FaCameraRetro, FaMoneyBillAlt, FaEye } from "react-icons/fa";
 import Title from "./Title";
 export default class Services extends Component {
   state = {
     services: [
       {
-        icon: <FaCocktail />,
-        title: "Free Cocktails",
+        icon: <FaCameraRetro />,
+        title: "Free photographer",
         info:
-          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias molestias eius libero?"
+          "We provide our customers a photographer from us to photograph the whole structure."
       },
       {
-        icon: <FaHiking />,
-        title: "Endless Hiking",
+        icon: <FaVrCardboard />,
+        title: "VR Services",
         info:
-          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias molestias eius libero?"
+          "We have the best 360 technology."
       },
       {
-        icon: <FaShuttleVan />,
-        title: "Free Shuttle",
+        icon: <FaMoneyBillAlt />,
+        title: "Low cost advertising",
         info:
-          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias molestias eius libero?"
+          "Just 1 Dollar to post your apartment."
       },
       {
-        icon: <FaBeer />,
-        title: "Strongest Beer",
+        icon: <FaEye />,
+        title: "Significant exposure",
         info:
-          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias molestias eius libero?"
+          "Advertising the structure on our site earns the best advertising there is."
       }
     ]
   };
