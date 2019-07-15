c20e747da96122b55edde3a93bfcc0e97d322218
roee030 <roee.angel93@gmail.com>
2019-07-15 20:24:13 +0300

    add banner to all pages

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
 function Error() {
     return (
-        <Hero/>
+        <Hero>
+        <Banner title="Error 404" subtitle="Page not found please return to safe zone">
+        <Link to="/" className="btn-primary">
+        Return Home
+        </Link>
+        </Banner>
+        </Hero>
     )
 }
 export default Error;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import Hero from "../components/Hero";
 import Banner from '../components/Banner';
- function Home() {
+function Home() {
     return (
         <Hero hero="defaultHero">
-        <Banner title="360° Homes" subtitle="Click here to view all apartments">
-        <Link to="/rooms" className="btn-primary">
-        Our Homes
-        </Link>
-        </Banner>
+            <Banner title="360° Homes" subtitle="Click here to view all apartments">
+                <Link to="/rooms" className="btn-primary">
+                    Our Homes
+                </Link>
+            </Banner>
         </Hero>
     );
 }
