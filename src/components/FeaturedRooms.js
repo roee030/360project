6ab473d0adc9ffbb3f858a3f75bbe0b5dc5658b4
roee030 <roee.angel93@gmail.com>
2019-07-19 18:09:12 +0300

    Update FeaturedRooms.js
    add value and check the room provider
    the date getting from context

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-
+import {RoomContext} from '../context';
 export default class FeaturedRooms extends Component {
+    static contextType = RoomContext;
     render() {
+        const value = this.context;
+        console.log(value);
         return (
             <div>
-                Hello from FeaturedRooms
+                Hello from FeaturedRooms {value}
             </div>
         )
     }
