f45ff2ac3a8bed567fb785fa70aafa8fb7a33194
roee030 <roee.angel93@gmail.com>
2019-07-21 23:21:07 +0300

    server setup
    mongodb server on port 5000
    now is the time to play

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://roee030:$A207186449@cluster0-yf7pj.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
