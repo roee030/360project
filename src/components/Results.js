91d1d5d94630b798f9c0c2b9ab02145d5bcf9d7e
roee030 <roee.angel93@gmail.com>
2019-07-23 17:10:45 +0300

    Update Results.js
    It works!!
    lets style the table

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-
+const ResultTable = props => (
+  <tr>
+    <td>{props.ResultTable.floor}</td>
+    <td>{props.ResultTable.floor}</td>
+    <td>{props.ResultTable.floor}</td>
+    <td>{props.ResultTable.numofRoom}</td>
+    <td>{props.ResultTable.price}</td>
+    <td>{props.ResultTable.description}</td>
+  </tr>
+)
 export default class Rooms extends Component {
     constructor(props) {
         super(props);
@@ -18,13 +27,28 @@ export default class Rooms extends Component {
             console.log("error fetch data from mongodb");
           })
       }
-     
+      exerciseList() {
+        return this.state.apartments.map(currentapartments => {
+          return <ResultTable ResultTable={currentapartments} key={currentapartments._id}/>;
+        })
+      }
     render() {
         return (
           <div>
           <h3>Logged Exercises</h3>
-       
-          
+          <table className="table">
+            <thead className="thead-light">
+              <tr>
+                <th>Username</th>
+                <th>Description</th>
+                <th>Duration</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              { this.exerciseList() }
+            </tbody>
+          </table>
         </div>
         )
     }
