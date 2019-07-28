3b3036890b748fbc634f41b1251aa3c17f1b49fc
roee030 <roee.angel93@gmail.com>
2019-07-28 13:16:56 +0300

    Update input.js
    add drop down options for rent and buy
    also sreach btn

--- src/components/input.js
+++ src/components/input.js
@@ -3,12 +3,22 @@ import React, { Component } from 'react'
 export default class input extends Component {
     render() {
         return (
+            <>
             <div class="input-group mb-3">
+            
             <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroup-sizing-default">Default</span>
+              <button class="btn btn-outline-secondary" type="button" id="button-addon1">Button</button>
             </div>
-            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"></input>
+            <input type="text" class="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1"></input>
+            <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Property Type</button>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Buy</a>
+              <div role="separator" class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Rent</a>
             </div>
+            </div>
+            
+          </>
         )
     }
 }
