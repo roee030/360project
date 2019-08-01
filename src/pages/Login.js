478f1c29c8d598a84cce598006c462ef1618fe4b
roee030 <roee.angel93@gmail.com>
2019-08-01 19:15:56 +0300

    style the login page
    add css and fixed problems

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react'
-
+import LoginBox from '../components/LoginBox';
+import RegisterBox from '../components/RegisterBox';
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          isLoginOpen: true,
+          isRegisterOpen: false
+        };
+      }
+      showRegisterBox() {
+          this.setState({isRegisterOpen: true, isLoginOpen: false})
+      }
+      showLoginBox() {
+        this.setState({isRegisterOpen: false, isLoginOpen: true})
+    }
     render() {
         return (
             <div className="root-container">
-                <div className="box-controller">
-                    <div className="controller">
-                    Login
-                    </div>
-                    <div className="controller">
-                    Register
+             
+                    <div className="box-controller"> 
+                        <div className="controller" onClick={this.showLoginBox.bind(this)}> 
+                        Login
+                        </div>
+                        <div className="controller"onClick={this.showRegisterBox.bind(this)}> 
+                        Register
+                        </div>
                     </div>
                     <div className="box-container">
+                    
                     </div>
-                </div>
+                    
             </div>
         )
     }
