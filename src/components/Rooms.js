3315da96459dde9b9c3d9a47cde3ab2ef4952410
roee030 <roee.angel93@gmail.com>
2019-08-22 23:41:39 +0300

    add default img

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
+import defaultImg from '../images/roee-Room'
 export default function Rooms({room}) {
     const{name,slug,images,price} = room;
     return( <article className="room">
     <div className="img-container">
-        <img src={images[0]} alt="single room" />
+        <img src={images[0] || defaultImg } alt="single room" />
     </div>
     </article>
     );
