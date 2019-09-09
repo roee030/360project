03b630f2648572b9a7bbec34e8b63bcf0bc6e40d
roee030 <roee.angel93@gmail.com>
2019-09-09 19:37:05 +0300

    set cover

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -3,13 +3,15 @@ import {Link} from 'react-router-dom';
 import defaultImg from '../images/Ariel_Home.jpg'
 
 export default function Rooms({room}) {
-    const{name,slug,images,price,iframeSrc,frontImgSrc} = room;
+    console.log({room})
+    const{description,price,iframeSrc,frontImgSrc} = room;
+    
     return( 
+        
     <article className="room">
-        {name}
+        {description}
         <div className="img-container">
         <img src={ frontImgSrc } alt="single room" />
-        <div id="myPanoViewer"></div>
         </div>
     </article>
     );
