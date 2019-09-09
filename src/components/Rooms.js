4d1ddcf32e5529b3fe1d8b327d00e4a8074db3be
roee030 <roee.angel93@gmail.com>
2019-09-09 19:56:36 +0300

    add style to room components

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -4,17 +4,19 @@ import defaultImg from '../images/Ariel_Home.jpg'
 
 export default function Rooms({room}) {
     console.log({room})
-    const{description,price,iframeSrc,frontImgSrc} = room;
+    const{description,city,price,iframeSrc,frontImgSrc} = room;
     
     return( 
         
     <article className="room">
-        {description}
+        
         <div className="img-container">
-        <img src={ frontImgSrc } alt="single room" />
-        <div className="price-top">
-        <h6>{price}</h6>
-        </div>
+            <img src={ frontImgSrc } alt="single room" />
+            <div className="price-top">
+                <h6>
+                    {city}
+                </h6>
+            </div>
         </div>
     </article>
     );
