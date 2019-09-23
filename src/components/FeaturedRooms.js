00b281cd13d5ec2f5db15057009cf8cd3c4b939a
roee030 <roee.angel93@gmail.com>
2019-09-23 15:24:08 +0300

    Update FeaturedRooms.js

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react'
-import {RoomContext} from '../context';
+import { RoomContext } from '../context';
 import Loading from '../components/Loading';
 import Room from './Room';
 import Title from '../components/Title';
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext;
-    
+
     render() {
-        let {rooms,loading} = this.context;    
-        rooms = rooms.map(room=>{
-            if(room.featured === true)
-            {
-                return <Room key={room.id} room={room}/>
+        let { rooms, loading } = this.context;
+        rooms = rooms.map(room => {
+            if (room.featured === true) {
+                return <Room key={room.id} room={room} />
             }
         })
         return (
             <section className="featured-rooms">
-                <Title title="featured-rooms"/>
+                <Title title="featured-rooms" />
                 <div className="featured-rooms-center">
-                {loading? <Loading/> : rooms}
+                    {loading ? <Loading /> : rooms}
                 </div>
             </section>
         )
