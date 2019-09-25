c7462e5257bf8156cbf64582e9062ada946bb48e
roee030 <roee.angel93@gmail.com>
2019-09-25 16:35:38 +0300

    Update SingleRoom.js
    update the single room page
    if there is no room from the context it will send him back to the room page.

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react'
-
+import {RoomContext} from '../context';
 class SingleRoom extends Component {
+    static contextType = RoomContext;
+    constructor(props)
+    {
+        super(props);
+        this.state={
+            id:this.props.match.params.id
+        }
+    }
     render() {
-        return (
+        const {getRoom} = this.context;
+        const room = getRoom(this.state.id);
+        if (!room) {
+            return (
+              <div className="error">
+                <h3> no such room could be found...</h3>
+                <Link to="/rooms" className="btn-primary">
+                  back to rooms
+                </Link>
+              </div>
+            );
+          }        return (
             <div>
                <h1> hello from single room page</h1>
             </div>
