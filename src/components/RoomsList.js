bfaf53eed11e34d20fdcf91ae31be2e1f906620a
roee030 <roee.angel93@gmail.com>
2019-10-03 20:56:13 +0300

    Update RoomsList.js

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,9 +1,23 @@
-import React from 'react'
-
-export default function RoomsList() {
+import React from "react";
+import Room from "./Room";
+const RoomsList = ({ rooms }) => {
+  if (rooms.length === 0) {
     return (
-        <div>
-            Hello from RoomList
-        </div>
-    )
-}
+      <div className="empty-search">
+        <h3>unfortunately no rooms matched your search parameters</h3>
+      </div>
+    );
+  }
+  console.log(rooms)
+  return (
+    <section className="roomslist">
+      <div className="roomslist-center">
+        {rooms.map(item => {
+          return <Room key={item.id} room={item} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default RoomsList;
